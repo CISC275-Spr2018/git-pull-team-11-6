124936d0303cb8e5e99c3ac8d4de6e7d45821915
kburd <kburd@udel.edu>
2018-04-02 22:48:53 -0400

    Renamed Animation to Controller and Draw to View

--- src/Controller.java
+++ src/Controller.java
@@ -1,30 +1,45 @@
-/**
- * Do not modify this file without permission from your TA.
- **/
-public class Controller {
 
-	private Model model;
-	private View view;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class Controller extends JFrame {
+    
+    Action drawAction;
+    final int drawDelay = 30; //msec
+    
+    View view = new View();
+    Model model = new Model(view.getWidth(), view.getHeight(), view.getImageWidth(), view.getImageHeight());
+
+    public Controller() {
+    	drawAction = new AbstractAction(){
+    		public void actionPerformed(ActionEvent e){
+                model.updateLocationAndDirection();
+    			view.update(model.getX(), model.getY(), model.getDirect());
+    		}
+    	};
+    	
+    	
+        add(view);
+    	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	setSize(view.getWidth(), view.getHeight());
+    	setVisible(true);
+    	pack();
+    }
 	
-	public Controller(){
-		view = new View();
-		model = new Model(view.getWidth(), view.getHeight(), view.getImageWidth(), view.getImageHeight());
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable(){
+			public void run(){
+				Controller a = new Controller();
+				Timer t = new Timer(a.drawDelay, a.drawAction);
+				t.start();
+			}
+		});
 	}
-	
-        //run the simulation
-	public void start(){
-		for(int i = 0; i < 5000; i++)
-		{
-			//increment the x and y coordinates, alter direction if necessary
-			model.updateLocationAndDirection();
-			//update the view
-			view.update(model.getX(), model.getY(), model.getDirect());
-		}
-	}
-	
-	public static void main(String[] args){
-		Controller control = new Controller();
-		control.start();
-	}
-	
+    
 }
+
