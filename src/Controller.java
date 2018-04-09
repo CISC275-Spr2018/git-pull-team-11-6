289d3eca3a7f061d710142f7bc449a88b7695ceb
Nick Rife <nrife@udel.edu>
2018-04-09 18:03:49 -0400

    added keyboard arrows

--- src/Controller.java
+++ src/Controller.java
@@ -11,6 +11,7 @@ import javax.swing.Timer;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 import javax.swing.AbstractButton;
 import java.awt.FlowLayout;
@@ -34,19 +35,19 @@ public class Controller extends JFrame{
     Model model = new Model(view.getWidth(), view.getHeight(), view.getImageWidth(), view.getImageHeight());
 
     public Controller() {
-    	drawAction = new AbstractAction(){
-    		public void actionPerformed(ActionEvent e){
-    			// update based on whether the game is "paused"
-    			if (!model.paused) {
-	                model.updateLocationAndDirection();
-	    			view.update(model.getX(), model.getY(), model.getDirect());
-	    		}//if
-    		}//actionPerformed
-    	};//AbastractAction
+        drawAction = new AbstractAction(){
+            public void actionPerformed(ActionEvent e){
+                // update based on whether the game is "paused"
+                if (!model.paused) {
+                    model.updateLocationAndDirection();
+                    view.update(model.getX(), model.getY(), model.getDirect());
+                }//if
+            }//actionPerformed
+        };//AbastractAction
 
-    	// adding image features
+        // adding image features
 
-    	// add icons (can be used later on...)
+        // add icons (can be used later on...)
         // ImageIcon playIcon = new ImageIcon("./../images/play.png", "PLAY");
         // ImageIcon pauseIcon = new ImageIcon("./../images/pause.png", "PAUSE");
 
@@ -65,6 +66,7 @@ public class Controller extends JFrame{
                     System.out.println("pausing");
                     pauseButton.setText("PLAY");
                 }//else if
+                requestFocusInWindow();
             }//actionPerformed
         });//JButton pauseButton
 
@@ -74,6 +76,7 @@ public class Controller extends JFrame{
                     //Switching direction.
                     System.out.println("Changing direction from "+model.dir.getName()+" to "+model.dir.next().getName());
                     model.dir = model.dir.next();
+                    requestFocusInWindow();
             }//actionPerformed
         });//JButton dirButton
         
@@ -84,6 +87,14 @@ public class Controller extends JFrame{
                     System.out.println("Changing direction to EAST");
                     model.dir = Direction.EAST;
             }//actionPerformed
+            // @Override
+            // public void keyPressed(KeyEvent e) {
+            //     int key = e.getKeyCode();
+
+            //     if (key == KeyEvent.VK_LEFT) {
+            //         model.dir=Direction.EAST;
+            //     }
+            // }
         });//JButton dirButton
         
         WButton = new JButton(new AbstractAction("WEST"){
@@ -125,40 +136,82 @@ public class Controller extends JFrame{
         //dirButton.setMnemonic(KeyEvent.VK_D);
         
         // set action from button
-        // pauseButton.setActionCommand("pause");		// **MAY BE NEEDED LATER ON...**
+        // pauseButton.setActionCommand("pause");       // **MAY BE NEEDED LATER ON...**
 
         // for some reason, view has to be added AFTER pauseButton to be able to see the button...
         
         buttonContainer = new JPanel();
         buttonContainer.setBackground(view.buttonBarBG);
 
-        buttonContainer.add(pauseButton);
-        buttonContainer.add(dirButton);
-        buttonContainer.add(EButton);
-        buttonContainer.add(WButton);
-        buttonContainer.add(SButton);
-        buttonContainer.add(NButton);
+        add(pauseButton);
+        add(dirButton);
+        // buttonContainer.add(EButton);
+        // buttonContainer.add(WButton);
+        // buttonContainer.add(SButton);
+        // buttonContainer.add(NButton);
+
+        setFocusable(true);
+        addKeyListener( new KeyListener() {
+
+            @Override
+            public void keyTyped( KeyEvent e ) {}
+
+            @Override
+            public void keyPressed( KeyEvent e ) {
+                int key = e.getKeyCode();
+
+                switch (key) {
+                    case (KeyEvent.VK_LEFT):
+                        model.dir=Direction.WEST;
+                        break;
+                    
+                    case (KeyEvent.VK_UP):
+                        model.dir=Direction.NORTH;
+                        break;
+
+                    case (KeyEvent.VK_RIGHT):
+                        model.dir=Direction.EAST;
+                        break;
+
+                    case (KeyEvent.VK_DOWN):
+                        model.dir=Direction.SOUTH;
+                        break;
+                }
+
+                if (key == KeyEvent.VK_F) {
+                    // do stuff
+                }
+                else if (key == KeyEvent.VK_J) {
+                    // do stuff
+                }
+            }
+
+            @Override
+            public void keyReleased( KeyEvent e ) {}
+
+    } );
+
         view.add(buttonContainer);
         add(view);
         
         // add the settings
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());				// i added this (helps display the button correctly)
-    	setSize(view.getWidth(), view.getHeight());
-    	setVisible(true);
-    	// pack();		// sets frame to its "preferred" size (doesn't help when using a button)
-    	
+        setLayout(new FlowLayout());                // i added this (helps display the button correctly)
+        setSize(view.getWidth(), view.getHeight());
+        setVisible(true);
+        // pack();      // sets frame to its "preferred" size (doesn't help when using a button)
+        
     }//Controller Constructor
     
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable(){
-			public void run(){
-				Controller a = new Controller();
-				Timer t = new Timer(a.drawDelay, a.drawAction);
-				t.start();
-			}//run
-		});//runnable
-	}//main
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable(){
+            public void run(){
+                Controller a = new Controller();
+                Timer t = new Timer(a.drawDelay, a.drawAction);
+                t.start();
+            }//run
+        });//runnable
+    }//main
     
 }//Controller
 
